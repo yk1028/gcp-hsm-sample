7ba4b194b40593a99e25dbea47d9a2da9f93040b
songwongeun <wongeunsong94@gmail.com>
2022-09-01 16:59:06 +0900

    docs: add comment

--- src/hsm/GcpHsmKey_xpla.ts
+++ src/hsm/GcpHsmKey_xpla.ts
@@ -1,3 +1,5 @@
+// xpla를 사용하는 경우 GcpHsmKey.ts 파일을 이 파일로 변경 필요
+
 import { Key, SimplePublicKey } from '@xpla/xpla.js';
 import { GcpHsmSigner } from './GcpHsmSigner';
 import { keccak256 } from '@ethersproject/keccak256';
